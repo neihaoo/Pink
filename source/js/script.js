fb76784808f52d090d6eb54c42e34dec10babcf6
neihaoo <neihaoo@gmail.com>
2018-02-12 12:18:56 +0300

    Вносит исправления в верстку

--- source/js/script.js
+++ source/js/script.js
@@ -19,3 +19,16 @@ navToggle.addEventListener('click', function() {
 // Reviews slider
 // Price slider
 // Map
+function initMap() {
+  var pink = {lat: 59.936162, lng: 30.321043};
+  var center = {lat: 59.936162, lng: 30.321043};
+  var map = new google.maps.Map(document.querySelector('.contacts__map-google'), {
+    zoom: 16,
+    center: center
+  });
+  var marker = new google.maps.Marker({
+    position: pink,
+    map: map,
+    icon: 'img/icon-map-marker.svg'
+  });
+}
